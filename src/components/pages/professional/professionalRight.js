9e8deaffd96486ff1fa8b34f2d2328f3b9db9735
thejhubbs <thejhubbs@gmail.com>
2020-11-16 16:20:07 -0500

    Repolished a few basic pages

--- src/components/pages/professional/professionalRight.js
+++ src/components/pages/professional/professionalRight.js
@@ -38,49 +38,7 @@ class ProfessionalRight extends React.Component {
 
 
     render() {
-        var data = [
-            {
-                year: "2011-2014",
-                title: "The Ohio State University",
-                role: "Computer Science & Engineering",
-                image: "ohiostatelogo.png",
-                description: "Engineering 101, Calculus, Algorithms, Low Level Languages (C++, C, Assembly) & Data Structures, Business & Marketing Electives"
-            },
-            {
-                year: "2013-2015",
-                title: "Startup Developer",
-                role: "Web Developer",
-                description: "HTML, CSS, JS, JQuery, PHP, SQL, Ruby on Rails"
-            },
-            {
-                year: "2011-2017",
-                title: "Arby's",
-                role: "Shift Manager",
-                description: "I originally got the job out of high school. Responsible for customer service & money handling, I matured here to the point of being Shift Leader my last two years."
-            },
-            {
-                year: "2017-Current",
-                title: "JLC Landscaping",
-                role: "Team Leader",
-                description: "I quickly obtained a leadership position within months of joining the team, due to my management experience & work ethic. Responsible for running lawn-cutting routes & yearly maintenance, I really learned the value of attention to detail and being proud of your work."
-            },
-            {
-                year: "2019-Current",
-                title: "Upwork",
-                role: "Freelancer",
-                link: "https://www.upwork.com/",
-                image: "upwork_icon.png",
-                description: "I continued doing freelance work this year, obtaining an average of 5/5 stars on reviews."
-            },
-            {
-                year: "2019-Current",
-                title: "Lambda School",
-                role: "Full Stack Web Developer",
-                link: "https://lambdaschool.com/",
-                image: "lambdalogo.png",
-                description: "React/Redux, Node/Express, creating a full product with a team. I'm currently attending Lambda School to cement my portfolio & technical skills to get a job."
-            },
-        ]
+        var data = []
 
 
 
@@ -135,23 +93,7 @@ class ProfessionalRight extends React.Component {
 
 
 
-                  <h2 className="pageSeparatorHeader" style={{textDecoration:"underline", marginTop:"25px"}}>Work & Education History</h2>
-                  <div id="resumeBottom">
-
-                  {data.reverse().map(item => <div className="workHistoryItem">
-                    <h4 style={{textDecoration:"underline"}}>{item.title}</h4>
-                    {item.image ? <span><img className="resumeImage" src={require(`../../../img/${item.image}`)} /><br /></span> : ""}
-                    <h5>{item.role} ({item.year})</h5>
-                    <p>{item.description}</p>
-                    {item.link ? <a className="resumeLink" href={item.link} target="_blank">More Information about {item.title}</a> : ""}
-                    </div>
-                )}
-
-
-
-                <Link className="page-button d-block d-sm-none" onClick={() => window.scrollTo(0, 0)} to="/portfolio">My Portfolio</Link>
-                <Link className="page-button d-block d-sm-none" style={{marginTop:"5px"}} onClick={() => window.scrollTo(0, 0)} to="/contact">Contact</Link>
-            </div></div>
+                  </div>
     }
 }
 
